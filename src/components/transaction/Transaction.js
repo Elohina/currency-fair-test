2bc2d64d013a0a2d93fdaca163f1ceb56b65d20c
Elohina Guevara <elohina@Elohinas-MacBook-Pro.local>
2018-11-14 23:26:56 +0000

    Add Tabs styles and transactions

--- src/components/transaction/Transaction.js
+++ src/components/transaction/Transaction.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Transaction.css";
 
 import Tabs from "../common/Tabs/Tabs";
 import Tab from "../common/Tabs/Tab";
@@ -23,20 +24,13 @@ class Transaction extends React.Component {
             }
         ];
         return(
-            <div className="wrapper transaction-steps">
+            <div className="wrapper">
                 <Tabs items={tabs_list}/>
-                {/* <Tab id="stepOne">
-                    <h4 className="tab-step">Step 1</h4>
-                    <h2 className="tab-title">Transaction Info</h2>
-                </Tab>
-                <Tab id="stepTwo"></Tab>
-                    <h4 className="tab-step">Step 2</h4>
-                    <h2 className="tab-title">Recipient info</h2>
-                <Tab id="stepThree">
-                    <h4 className="tab-step">Step 3</h4>
-                    <h2 className="tab-step">Make payment</h2>
-                </Tab> */}
-
+                    <Tab id="stepOne">
+                        <h3>Let’s set up your transaction!</h3>
+                        <h4>Specify the amount to be sent or received.</h4>
+                        <button type="button" className="button-next">Next</button>
+                    </Tab>
             </div>
         );
     }
